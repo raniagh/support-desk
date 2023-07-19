b0431f10aaaa53123c5d41aaa3fc1084d3e533bb
Your Name <rghzaiel@ggn-group.com>
2023-07-19 13:37:55 +0100

    connect to db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,25 @@
-const registerUser = (req, res) => {
-  res.send("Register route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+//@desc Register a new user
+//@route /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+  res.send("Register route");
+});
+
+//@desc Login user
+//@route /api/users/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login route");
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db.js");
 const PORT = process.env.PORT || 5000;
 
+//Connect to database
+connectDB();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
