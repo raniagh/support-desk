7c53daf36c4690f8f96aa10b013ad4b305280f65
Your Name <rghzaiel@ggn-group.com>
2023-07-19 12:20:44 +0100

    add routes & controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
