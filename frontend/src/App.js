f894d702928464c0715e6902fc8883690d5621dc
Your Name <rghzaiel@ggn-group.com>
2023-07-20 11:52:40 +0100

    register login home UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <Router>
+        <div class="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
