6bcdf3a0b82f27f639b49ac71c4ec88270458f28
Your Name <rghzaiel@ggn-group.com>
2023-07-20 10:19:00 +0100

    frontend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    //here the Colors module is used
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Eroor: ${error.message}`.red.underline.bold);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
