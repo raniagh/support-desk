5411fd7df167e14a6b33fe9d35cc31540e4d14a9
Your Name <rghzaiel@ggn-group.com>
2023-07-24 16:50:51 +0100

    update ticket status

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { getTicket } from "../features/tickets/ticketSlice";
+import { closeTicket, getTicket } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
 
@@ -10,9 +10,9 @@ const Ticket = () => {
   const { isError, isLoading, ticket, message } = useSelector(
     (state) => state.ticket
   );
-
   const { ticketId } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isError) {
@@ -21,6 +21,13 @@ const Ticket = () => {
 
     dispatch(getTicket(ticketId));
   }, [isError, message, dispatch, ticketId]);
+
+  //Close ticket
+  const onCloseTicket = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket Closed");
+    navigate("/tickets");
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -35,7 +42,7 @@ const Ticket = () => {
       <header className="ticket-header">
         <BackButton url="/" />
         <h2>
-          Ticket ID: {ticket._id}{" "}
+          Ticket ID: {ticket._id}
           <span className={`status status-${ticket.status}`}>
             {ticket.status}
           </span>
@@ -43,12 +50,19 @@ const Ticket = () => {
         <h3>
           Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-US")}
         </h3>
+        <h3>Product: {ticket.product}</h3>
         <hr />
         <div className="ticket-desc">
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
       </header>
+
+      {ticket.status !== "closed" && (
+        <button className="btn btn-block btn-danger" onClick={onCloseTicket}>
+          Close Ticket
+        </button>
+      )}
     </div>
   );
 };
