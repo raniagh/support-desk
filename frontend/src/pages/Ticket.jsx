a7904557511e6bb7abd96e39d55970162d6c77a9
Your Name <rghzaiel@ggn-group.com>
2023-07-25 11:35:36 +0100

    notes list UI

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -5,10 +5,16 @@ import { toast } from "react-toastify";
 import { closeTicket, getTicket } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import { getNotes } from "../features/notes/noteSlice";
+import NoteItem from "../components/NoteItem";
 
 const Ticket = () => {
   const { isError, isLoading, ticket, message } = useSelector(
     (state) => state.ticket
+  );
+
+  const { isLoading: notesIsLoading, notes } = useSelector(
+    (state) => state.note
   );
   const { ticketId } = useParams();
   const dispatch = useDispatch();
@@ -20,6 +26,7 @@ const Ticket = () => {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
   }, [isError, message, dispatch, ticketId]);
 
   //Close ticket
@@ -29,7 +36,7 @@ const Ticket = () => {
     navigate("/tickets");
   };
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -56,8 +63,11 @@ const Ticket = () => {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
-
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
       {ticket.status !== "closed" && (
         <button className="btn btn-block btn-danger" onClick={onCloseTicket}>
           Close Ticket
